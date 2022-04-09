cd0691d81c5e60d2f6e2269e60fe4ef6cf2182df
likewu <likewu@126.com>
2022-04-09 22:12:50 +0800

    aa

--- qemu.sh
+++ qemu.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # You might need to change this...
-ESP_QEMU_PATH=../qemu/build
+ESP_QEMU_PATH=/home/leafcolor/app/qemu-esp-develop-20220203/qemu/bin
 BUILD=debug
 
 TARGET=xtensa-esp32-espidf # Don't change this. Only the ESP32 chip is supported in QEMU for now
 
+#D:\tools\esptool-v3.3-win64\esptool
 esptool.py --chip esp32 elf2image target/$TARGET/$BUILD/rust-esp32-std-hello
 esptool.py --chip esp32 merge_bin --output target/$TARGET/$BUILD/rust-esp32-std-hello-qemu.bin --fill-flash-size 4MB 0x1000 qemu_bins/bootloader.bin  0x8000 qemu_bins/partitions.bin  0x10000 target/$TARGET/$BUILD/rust-esp32-std-hello.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
 $ESP_QEMU_PATH/qemu-system-xtensa -nographic -machine esp32 -nic user,model=open_eth,id=lo0,hostfwd=tcp:127.0.0.1:7888-:80 -drive file=target/$TARGET/$BUILD/rust-esp32-std-hello-qemu.bin,if=mtd,format=raw
+#
